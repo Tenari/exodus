51579aed3eb5ccb999c6447bae465cdd1af97de3
Daniel Zapata <daniel@peoplepattern.com>
2019-04-15 17:12:56 -0500

    handle newlines

--- app/assets/javascripts/components/today.js.jsx
+++ app/assets/javascripts/components/today.js.jsx
@@ -1,10 +1,14 @@
 var Today = createReactClass({
   render(){
     var day = this.props;
+    var ps = [];
+    _.each(day.reading.split("\n"), function(p){
+      ps.push(<p>{p}</p>);
+    })
     return <div className="day-summary">
       <h2>{day.date}</h2>
       <h4>Reading</h4>
-      <p>{day.reading}</p>
+      {ps}
       <h4>Ascetic practices</h4>
       <ul>
         {_.map(day.practices, function(practice){
